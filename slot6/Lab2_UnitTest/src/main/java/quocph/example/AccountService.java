0422088f175c8db6e850a4c3c9e8d85e97da2382
phWiz-2412 <quocne24@gmail.com>
2026-01-26 08:02:21 +0700

    Initial commit - SWT301 slots

---
+++ slot6/Lab2_UnitTest/src/main/java/quocph/example/AccountService.java
@@ -0,0 +1,16 @@
+package quocph.example;
+
+public class AccountService {
+
+    public boolean registerAccount(String username, String password, String email) {
+        if (username == null || username.isEmpty()) return false;
+        if (password == null || password.length() <= 6) return false;
+        if (!isValidEmail(email)) return false;
+        return true;
+    }
+
+    public boolean isValidEmail(String email) {
+        if (email == null) return false;
+        return email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    }
+}
