f19b3637fae550a0fae47b13996c080d0c539c82
phWiz-2412 <quocne24@gmail.com>
2026-03-16 07:19:18 +0700

    Update CMS Automation Test project

--- Exercise1_SeleniumBasic/src/test/java/traltb/fe/edu/vn/LoginTest.java
+++ Exercise1_SeleniumBasic/src/test/java/traltb/fe/edu/vn/LoginTest.java
@@ -1,5 +1,9 @@
+package traltb.fe.edu.vn;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,8 +13,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -33,8 +35,11 @@ public class LoginTest {
         driver.manage().window().maximize();
     }
 
+    // ================= BASIC TEST =================
+
     @Test
     @Order(1)
+    @DisplayName("Login Success")
     void testLoginSuccess() {
 
         driver.get("https://the-internet.herokuapp.com/login");
@@ -52,11 +57,12 @@ public class LoginTest {
 
     @Test
     @Order(2)
+    @DisplayName("Login Fail - Wrong Username")
     void testLoginFail() {
 
         driver.get("https://the-internet.herokuapp.com/login");
 
-        driver.findElement(By.id("username")).sendKeys("invalid");
+        driver.findElement(By.id("username")).sendKeys("wronguser");
         driver.findElement(By.id("password")).sendKeys("wrongpassword");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
 
@@ -64,8 +70,47 @@ public class LoginTest {
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".flash.error"))
         );
 
-        assertTrue(errorMsg.getText().contains("Your username is invalid!"));
+        assertTrue(errorMsg.getText().toLowerCase().contains("invalid"));
     }
+
+    // ================= PARAMETERIZED TEST =================
+
+    @Order(3)
+    @ParameterizedTest(name = "Test Login - Username: {0}, Password: {1}")
+    @CsvSource({
+            "tomsmith, SuperSecretPassword!, success",
+            "wronguser, SuperSecretPassword!, error",
+            "tomsmith, wrongpassword, error",
+            "'', '', error"
+    })
+    @DisplayName("Multiple login attempts using @CsvSource")
+    void testLoginWithMultipleParameters(String username, String password, String expectedResult) {
+
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("password")).clear();
+
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        By messageLocator = expectedResult.equals("success")
+                ? By.cssSelector(".flash.success")
+                : By.cssSelector(".flash.error");
+
+        WebElement message = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(messageLocator)
+        );
+
+        if (expectedResult.equals("success")) {
+            assertTrue(message.getText().contains("You logged into a secure area!"));
+        } else {
+            assertTrue(message.getText().toLowerCase().contains("invalid"));
+        }
+    }
+
+    // ================= TEARDOWN =================
 
     @AfterAll
     static void tearDown() {
